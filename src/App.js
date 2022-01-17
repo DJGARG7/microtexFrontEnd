2d67916e92b7e60cf45cafb0a617c1cb70924910
dh711 <dhairya711@gmail.com>
2022-01-17 15:44:08 +0400

    updated css.

--- src/App.js
+++ src/App.js
@@ -4,134 +4,16 @@ import { useEffect, useState } from "react";
 import Login from "./components/Login";
 var CryptoJS = require("crypto-js");
 function App() {
-<<<<<<< HEAD
-  var data = [{ name: 'Rutvay',u_id:'RUT0051' }, { name: 'Digvi',u_id:'DRI0058' }]
-  localStorage.setItem("savedPro",JSON.stringify(data))
-  return (
-    <div className="mainParent">
-    <div className="logo">MicroTex ERP Solutions</div>
-    {isLogged? (<Login/>):(<adminDashboard/>)}
-     <div className="navbar">
-        <a href="#home" class="active">support@microtex.in</a>
-        <a>Contact: 1800 5654 7868</a>
-        <a href="#news">Date</a>
-        <a href="#contact">Time</a>
-      </div>
-    </div>
-  );
-=======
-    const [savedList, setSavedList] = useState(
-        JSON.parse(localStorage.getItem("saved"))
-    );
-    const [corpId, setCorpId] = useState();
-    const [userId, setUserId] = useState();
     var data = [
-        { c_id: "KSM0001", name: "Dhananjay", u_id: "DJG0065" },
-        { c_id: "SUF0001", name: "Dhairya", u_id: "DIR0007" },
+        { name: "Rutvay", u_id: "RUT0051" },
+        { name: "Digvi", u_id: "DRI0058" },
     ];
-    localStorage.setItem("saved", JSON.stringify(data));
-    const savedClickHandler = (c_id, u_id) => {
-        console.log(c_id, u_id);
-        setCorpId(c_id);
-        setUserId(u_id);
-    };
-    console.log(savedList);
+    localStorage.setItem("savedPro", JSON.stringify(data));
     return (
         <div className="mainParent">
-            <div className="logo">
-                <h1>MicroTex ERP Solutions</h1>
-            </div>
-            <div className="blackbox">
-                <div className="leftPart">
-                    <h2>Saved Users</h2>
-                    <div className="recentUserList">
-                        {Object.keys(savedList).map((ele) => {
-                            return (
-                                <button
-                                    onClick={() =>
-                                        savedClickHandler(
-                                            savedList[ele].c_id,
-                                            savedList[ele].u_id
-                                        )
-                                    }
-                                >
-                                    <table>
-                                        <tr>
-                                            <td rowSpan={2}>
-                                                {savedList[ele].name}
-                                            </td>
-                                            <td>{savedList[ele].c_id}</td>
-                                        </tr>
-                                        <tr>
-                                            <td>{savedList[ele].u_id}</td>
-                                        </tr>
-                                    </table>
-                                </button>
-                            );
-                        })}
-                    </div>
-                </div>
-                <div className="rightPart">
-                    <h2>Login</h2>
-                    <div>
-                        <form>
-                            <input
-                                type="text"
-                                name="cid"
-                                value={corpId}
-                                placeholder="Corporate ID"
-                            />
-                            <br />
-                            <input
-                                type="text"
-                                name="uid"
-                                value={userId}
-                                placeholder="User ID"
-                            />
-                            <br />
-                            <input
-                                type="password"
-                                name="password"
-                                value=""
-                                placeholder="Password"
-                            />
-                            <br />
-                            <input
-                                type="submit"
-                                className="btn-login"
-                                value="LOGIN"
-                            />
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-            {/* <div className="spacer">
-        <div className="title">Welcome</div>
-      </div>
-      <div className="form">
-        <form method="POST" action="/login/login.php" >
-          <div className="form-group text-field">
-            <input type="text" name="username" value="" class="" placeholder="Username"/><br/>
-            <div className="display-none"><p class='alert'></p></div>
-          </div>    
-          <div className="form-group text-field">
-            <input type="password" name="password" value="" class="" placeholder="Password"/><br/>
-            <div className="display-none"><p class='alert'></p></div>
-          </div>
-          <div className="form-group">
-            <div className="radio">
-                    <label className="radio-label"><input type="radio" name="role" value="student" class=""/> STUDENT</label>
-                    <label className="radio-label"><input type="radio" name="role" value="faculty" class=""/> FACULTY</label>
-                    <label className="radio-label"><input type="radio" name="role" value="admin" class=""/> ADMIN</label><br/><br/>
-                </div>
-                <div className="display-none"><p class='alert'></p></div>
-            </div>
-                        <div className="btn">
-                <input type="submit" className="btn-login" value="LOGIN"/>
-            </div>
-        </form>
-    </div> */}
+            <div className="logo">MicroTex ERP Solutions</div>
+            {/* {isLogged? (<Login/>):(<adminDashboard/>)} */}
+            <Login />
             <div className="navbar">
                 <a href="#home" class="active">
                     support@microtex.in
@@ -142,7 +24,6 @@ function App() {
             </div>
         </div>
     );
->>>>>>> responsive
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,121 +1,140 @@
 import { useEffect, useState } from "react";
 const Login = () => {
-  const [savedList, setSavedList] = useState(
-    JSON.parse(localStorage.getItem("saved"))
-  );
-  const [savedPro, setSavedPro] = useState(
-    JSON.parse(localStorage.getItem("savedPro"))
-  );
-  const [remember,setRemember] = useState(false);
+    const [savedList, setSavedList] = useState(
+        JSON.parse(localStorage.getItem("saved"))
+    );
+    const [savedPro, setSavedPro] = useState(
+        JSON.parse(localStorage.getItem("savedPro"))
+    );
+    const [remember, setRemember] = useState(false);
 
-  const [corpId, setCorpId] = useState();
-  const [userId, setUserId] = useState();
-  const [type, setType] = useState("firm");
-  const types = ["firm", "proprietor"];
-  const savedClickHandler = (c_id, u_id) => {
-    console.log(c_id, u_id);
-    setCorpId(c_id);
-    setUserId(u_id);
-    setType("firm");
-    setRemember(true);
-  };
-  const savedProClickHandler = (u_id) => {
-    console.log(u_id);
-    setUserId(u_id);
-    setType("proprietor");
-    setRemember(true);
-  };
-  const onChangeHandler = ()=>{
-      setRemember(!remember);
-  }
+    const [corpId, setCorpId] = useState();
+    const [userId, setUserId] = useState();
+    const [type, setType] = useState("firm");
+    const types = ["firm", "proprietor"];
+    const savedClickHandler = (c_id, u_id) => {
+        console.log(c_id, u_id);
+        setCorpId(c_id);
+        setUserId(u_id);
+        setType("firm");
+        setRemember(true);
+    };
+    const savedProClickHandler = (u_id) => {
+        console.log(u_id);
+        setUserId(u_id);
+        setType("proprietor");
+        setRemember(true);
+    };
+    const onChangeHandler = () => {
+        setRemember(!remember);
+    };
 
-  return (
-    <div className="blackbox">
-      <div className="leftPart">
-        Saved Users
-        <div>
-          <table>
-            {Object.keys(savedList).map((ele) => {
-              return (
-                <button
-                  onClick={() =>
-                    savedClickHandler(savedList[ele].c_id, savedList[ele].u_id)
-                  }
-                >
-                  <tr>
-                    <td rowSpan={2}>{savedList[ele].name}</td>
-                    <td>{savedList[ele].c_id}</td>
-                  </tr>
-                  <tr>
-                    <td>{savedList[ele].u_id}</td>
-                  </tr>
-                </button>
-              );
-            })}
-            {Object.keys(savedPro).map((ele) => {
-              return (
-                <button
-                  onClick={() => savedProClickHandler(savedPro[ele].u_id)}
-                >
-                  <tr>
-                    <td>{savedPro[ele].name}</td>
-                    <td>{savedPro[ele].u_id}</td>
-                  </tr>
-                </button>
-              );
-            })}
-          </table>
-        </div>
-      </div>
-      <div className="rightPart">
-        Login
-        <div>
-          <form>
-            <div>
-              {types.map((t) => (
-                <>
-                  <input
-                    type="radio"
-                    name="type"
-                    value={t}
-                    checked={type === t}
-                    onChange={(e) => setType(e.currentTarget.value)}
-                  />{" "}
-                  {t}
-                </>
-              ))}
-              <p>{type}</p>
+    return (
+        <div className="blackbox">
+            <div className="leftPart">
+                <h2>Saved Users</h2>
+                <div className="recentUserList">
+                    {Object.keys(savedList).map((ele) => {
+                        return (
+                            <button
+                                onClick={() =>
+                                    savedClickHandler(
+                                        savedList[ele].c_id,
+                                        savedList[ele].u_id
+                                    )
+                                }
+                            >
+                                <table>
+                                    <tr>
+                                        <td rowSpan={2}>
+                                            {savedList[ele].name}
+                                        </td>
+                                        <td>{savedList[ele].c_id}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>{savedList[ele].u_id}</td>
+                                    </tr>
+                                </table>
+                            </button>
+                        );
+                    })}
+                    {Object.keys(savedPro).map((ele) => {
+                        return (
+                            <button
+                                onClick={() =>
+                                    savedProClickHandler(savedPro[ele].u_id)
+                                }
+                            >
+                                <tr>
+                                    <td>{savedPro[ele].name}</td>
+                                    <td>{savedPro[ele].u_id}</td>
+                                </tr>
+                            </button>
+                        );
+                    })}
+                </div>
             </div>
-            {type === "firm" && (
-              <input
-                type="text"
-                name="cid"
-                value={corpId}
-                placeholder="Corporate ID"
-              />
-            )}
-            <br />
-            <input
-              type="text"
-              name="uid"
-              value={userId}
-              placeholder="User ID"
-            />
-            <br />
-            <input
-              type="password"
-              name="password"
-              value=""
-              placeholder="Password"
-            />
-            <br />
-            <input type="checkbox" checked={remember} onChange={onChangeHandler} name="remember" /> Remember me
-            <p>Is "My Value" checked? {remember.toString()}</p>
-            <input type="submit" className="btn-login" value="LOGIN" />
-          </form>
+            <div className="rightPart">
+                <h2>Login</h2>
+                <div>
+                    <form>
+                        <div>
+                            {types.map((t) => (
+                                <>
+                                    <input
+                                        type="radio"
+                                        name="type"
+                                        value={t}
+                                        checked={type === t}
+                                        onChange={(e) =>
+                                            setType(e.currentTarget.value)
+                                        }
+                                    />{" "}
+                                    {t}
+                                </>
+                            ))}
+                            <p>{type}</p>
+                        </div>
+                        {type === "firm" && (
+                            <input
+                                type="text"
+                                name="cid"
+                                value={corpId}
+                                placeholder="Corporate ID"
+                            />
+                        )}
+                        <br />
+                        <input
+                            type="text"
+                            name="uid"
+                            value={userId}
+                            placeholder="User ID"
+                        />
+                        <br />
+                        <input
+                            type="password"
+                            name="password"
+                            value=""
+                            placeholder="Password"
+                        />
+                        <br />
+                        <input
+                            type="checkbox"
+                            checked={remember}
+                            onChange={onChangeHandler}
+                            name="remember"
+                        />{" "}
+                        Remember me
+                        <p>Is "My Value" checked? {remember.toString()}</p>
+                        <input
+                            type="submit"
+                            className="btn-login"
+                            value="LOGIN"
+                        />
+                    </form>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 export default Login;
