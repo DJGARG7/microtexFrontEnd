94127e8eca4055584469486b10b28b8f4d39bbbe
DJGARG7 <18bce052@nirmauni.ac.in>
2022-02-24 18:29:03 +0530

    added CRUD buttons for account master

--- src/pages/AccountMaster.js
+++ src/pages/AccountMaster.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import AccountTypeData from "../jsonData/AccountTypeData";
 const AccountMaster = () => {
-    const [isEntering, setIsEntering] = useState(false);
+    const [isEntering, setIsEntering] = useState(true);
     const [accName, setAccName] = useState("");
     const [accType, setType] = useState("none");
     const [addline1, setAddLine1] = useState("");
@@ -10,8 +10,32 @@ const AccountMaster = () => {
     const [pinCode, setPinCode] = useState("");
     const [city, setCity] = useState("");
     const [boolList, setBoolList] = useState([]);
+    const [disMode, setDisMode] = useState(0);
 
-    //from database in useeffect
+    const buttonModes = {
+        0: [
+            { dis: true, label: "delete" },
+            { dis: false, label: "add" },
+            { dis: false, label: "view all data" },
+            { dis: true, label: "cancel" },
+            { dis: false, label: "exit" },
+        ],
+        1: [
+            { dis: false, label: "delete" },
+            { dis: true, label: "add" },
+            { dis: false, label: "edit" },
+            { dis: true, label: "cancel" },
+            { dis: false, label: "exit" },
+        ],
+        2: [
+            { dis: true, label: "delete" },
+            { dis: false, label: "save" },
+            { dis: true, label: "edit" },
+            { dis: false, label: "cancel" },
+            { dis: false, label: "exit" },
+        ],
+    };
+    //from city service in useeffect
     const CityData = ["Surat", "Delhi", "Mumbai", "Ahmedabad"];
 
     const submitHandler = (e) => {
@@ -28,6 +52,52 @@ const AccountMaster = () => {
         setCity(e.target.value);
     };
 
+    const deleteHandler = () => {
+        if (disMode == 1) {
+            // delete from database after confirming
+            console.log("deleted from database");
+            exitHandler();
+        }
+    };
+
+    const addSaveHandler = () => {
+        if (disMode == 0) {
+            // add to database
+            console.log("data added to db")
+        }
+        if (disMode == 2) {
+            // update to database
+            console.log("data updated to db")
+        }
+        setDisMode(1);
+        setIsEntering(false);
+    };
+
+    const editViewHandler = () => {
+        if (disMode == 0) {
+            // select * all data in modal
+            console.log("data displayed in modal")
+        }
+        if (disMode == 1) {
+            setDisMode(2);
+            setIsEntering(true);
+        }
+    };
+    const cancelHandler = () => {
+        if (disMode == 2) {
+            setDisMode(1);
+            //reset the values fetched from database
+            setIsEntering(false);
+        }
+    };
+    const exitHandler = () => {
+        // return to default screen like on reload
+        setDisMode(0);
+        setType("none");
+        setBoolList([]);
+        setIsEntering(true)
+    };
+    
     return (
         <div>
             <h1>This is Account Master</h1>
@@ -39,9 +109,15 @@ const AccountMaster = () => {
                     value={accName}
                     placeholder="Account Name"
                     onChange={(e) => setAccName(e.target.value)}
+                    disabled={!isEntering}
                     required
                 />
-                <select name="AccType" value={accType} onChange={changeHandler}>
+                <select
+                    name="AccType"
+                    value={accType}
+                    onChange={changeHandler}
+                    disabled={!isEntering}
+                >
                     <option value="none" selected disabled hidden>
                         Select AccType
                     </option>
@@ -58,6 +134,7 @@ const AccountMaster = () => {
                             placeholder="Address line1"
                             onChange={(e) => setAddLine1(e.target.value)}
                             required
+                            disabled={!isEntering}
                         />
                         <input
                             type="text"
@@ -66,6 +143,7 @@ const AccountMaster = () => {
                             placeholder="Address line2"
                             onChange={(e) => setAddLine2(e.target.value)}
                             required
+                            disabled={!isEntering}
                         />
                         <input
                             type="text"
@@ -74,11 +152,13 @@ const AccountMaster = () => {
                             placeholder="Address line3"
                             onChange={(e) => setAddLine3(e.target.value)}
                             required
+                            disabled={!isEntering}
                         />
                         <select
                             name="cityName"
                             value={city}
                             onChange={cityHandler}
+                            disabled={!isEntering}
                         >
                             <option value="none" selected disabled hidden>
                                 Select City
@@ -96,6 +176,7 @@ const AccountMaster = () => {
                             min={100000}
                             max={999999}
                             required
+                            disabled={!isEntering}
                         />
                     </div>
                 )}
@@ -109,6 +190,36 @@ const AccountMaster = () => {
 
                 <button>a</button>
             </form>
+            <button
+                disabled={buttonModes[disMode][0].dis}
+                onClick={deleteHandler}
+            >
+                {buttonModes[disMode][0].label}
+            </button>
+            <button
+                disabled={buttonModes[disMode][1].dis}
+                onClick={addSaveHandler}
+            >
+                {buttonModes[disMode][1].label}
+            </button>
+            <button
+                disabled={buttonModes[disMode][2].dis}
+                onClick={editViewHandler}
+            >
+                {buttonModes[disMode][2].label}
+            </button>
+            <button
+                disabled={buttonModes[disMode][3].dis}
+                onClick={cancelHandler}
+            >
+                {buttonModes[disMode][3].label}
+            </button>
+            <button
+                disabled={buttonModes[disMode][4].dis}
+                onClick={exitHandler}
+            >
+                {buttonModes[disMode][4].label}
+            </button>
         </div>
     );
 };
