0f1a060cd49e11d24753c89bfce3f9e9dd349fe2
dh711 <dhairya711@gmail.com>
2022-03-18 15:32:47 +0400

    fix some css for user dashboard.

--- src/components/Reuse_components/Sidebar/Sidebartest.js
+++ src/components/Reuse_components/Sidebar/Sidebartest.js
@@ -4,61 +4,59 @@ import UserSubmenu from "./UserSubmenu";
 import { IconContext } from "react-icons/lib";
 
 const SidebarNav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 
-  width: 17.5vw;
-  height: 100%;
-  overflow : auto;
-  background-color: #480ca8;
-  border-radius: 15px;
-  left: ${({ sidebar }) => (sidebar ? "0" : "-100%")};
-  transition: 350ms;
-  z-index: 10;
+    width: 17.5vw;
+    height: 100%;
+    overflow: auto;
+    background-color: #480ca8;
+    border-radius: 15px;
+
+    transition: 350ms;
+    z-index: 10;
 `;
 
-
 const SidebarWrap = styled.div`
-  width: 100%;
+    width: 100%;
 `;
 
 const Sidebartest = ({ SidebarDataUserTest, userDetails, logoutHandler }) => {
-  return (
-    <>
-      <IconContext.Provider value={{ color: "#fff" }}>
-        <SidebarNav sidebar={true}>
-        
-          <SidebarWrap>
-            {SidebarDataUserTest.map((item, index) => {
-              return <UserSubmenu item={item} key={index} />;
-            })}
-          </SidebarWrap>
-          <div className={styles["sidebar--user"]}>
-            <li
-              className={`${styles["sidebar--user-detail"]} ${styles["sidebar--user-name"]}`}
-            >
-              {userDetails.userName}
-            </li>
-            <li
-              className={`${styles["sidebar--user-detail"]} ${styles["sidebar--user-id"]}`}
-            >
-              {userDetails.corporateID !== ""
-                ? `${userDetails.corporateID} // ${userDetails.userID}`
-                : `${userDetails.userID}`}
-            </li>
+    return (
+        <>
+            <IconContext.Provider value={{ color: "#fff" }}>
+                <SidebarNav sidebar={true}>
+                    <SidebarWrap style={{ padding: "10px" }}>
+                        {SidebarDataUserTest.map((item, index) => {
+                            return <UserSubmenu item={item} key={index} />;
+                        })}
+                    </SidebarWrap>
+                    <div className={styles["sidebar--user"]}>
+                        <li
+                            className={`${styles["sidebar--user-detail"]} ${styles["sidebar--user-name"]}`}
+                        >
+                            {userDetails.userName}
+                        </li>
+                        <li
+                            className={`${styles["sidebar--user-detail"]} ${styles["sidebar--user-id"]}`}
+                        >
+                            {userDetails.corporateID !== ""
+                                ? `${userDetails.corporateID} // ${userDetails.userID}`
+                                : `${userDetails.userID}`}
+                        </li>
 
-            <button
-              className={styles["sidebar--logout-btn"]}
-              onClick={logoutHandler}
-            >
-              Logout
-            </button>
-          </div>
-        </SidebarNav>
-      </IconContext.Provider>
-    </>
-  );
+                        <button
+                            className={styles["sidebar--logout-btn"]}
+                            onClick={logoutHandler}
+                        >
+                            Logout
+                        </button>
+                    </div>
+                </SidebarNav>
+            </IconContext.Provider>
+        </>
+    );
 };
 
 export default Sidebartest;
--- src/components/Reuse_components/Sidebar/Usersubmenu2.js
+++ src/components/Reuse_components/Sidebar/Usersubmenu2.js
@@ -2,76 +2,79 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 const SidebarLink = styled(NavLink)`
-background: #414757;
-height: 60px;
-padding-left: 3rem;
-display: flex;
-align-items: center;
-text-decoration: none;
-color: #f5f5f5;
-font-size: 18px;
-&:hover {
-  background: #632ce4;
-  cursor: pointer;
-}
+    background: #414757;
+    height: 50px;
+    padding-left: 3rem;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #f5f5f5;
+    font-size: 18px;
+    &:hover {
+        background: #632ce4;
+        cursor: pointer;
+    }
 `;
 
 const SidebarLabel = styled.span`
-  margin-left: 16px;
+    margin-left: 0px;
+    color: white;
+    font-weight: 600;
+    font-size: 1rem;
+    letter-spacing: 1px;
+    text-decoration: none;
+    text-transform: uppercase;
 `;
 
 const DropdownLink = styled(NavLink)`
-  background: #414757;
-  height: 60px;
-  padding-left: 4rem;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  color: #f5f5f5;
-  font-size: 18px;
-  &:hover {
-    background: #632ce4;
-    cursor: pointer;
-  }
+    background: #414757;
+    height: 60px;
+    padding-left: 4rem;
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #f5f5f5;
+    font-size: 18px;
+    &:hover {
+        background: #632ce4;
+        cursor: pointer;
+    }
 `;
 function Usersubmenu2({ item }) {
     const [subnav, setSubnav] = useState(false);
 
     const showSubnav = () => setSubnav(!subnav);
-  
-  return(
-    <>
-    <SidebarLink to={item.path} onClick={item.subNav && showSubnav}>
-      <div>
-        {item.icon}
-        <SidebarLabel>{item.title}</SidebarLabel>
-      </div>
-      <div>
-        {item.subNav && subnav
-          ? item.iconOpened
-          : item.subNav
-          ? item.iconClosed
-          : null}
-      </div>
-    </SidebarLink>
-    {subnav &&
-      item.subNav.map((item, index) => {
-        return (
-          <div>
-            <DropdownLink
-              to={item.path}
-              key={index}
-            >
-              <div>
-                {item.icon}
-                <SidebarLabel>{item.title}</SidebarLabel>
-              </div>
-            </DropdownLink>
-          </div>
-        );
-      })}
-  </>
-  );
+
+    return (
+        <>
+            <SidebarLink to={item.path} onClick={item.subNav && showSubnav}>
+                <div>
+                    {item.icon}
+                    <SidebarLabel>{item.title}</SidebarLabel>
+                </div>
+                <div>
+                    {item.subNav && subnav
+                        ? item.iconOpened
+                        : item.subNav
+                        ? item.iconClosed
+                        : null}
+                </div>
+            </SidebarLink>
+            {subnav &&
+                item.subNav.map((item, index) => {
+                    return (
+                        <div>
+                            <DropdownLink to={item.path} key={index}>
+                                <div>
+                                    {item.icon}
+                                    <SidebarLabel>{item.title}</SidebarLabel>
+                                </div>
+                            </DropdownLink>
+                        </div>
+                    );
+                })}
+        </>
+    );
 }
 
 export default Usersubmenu2;
