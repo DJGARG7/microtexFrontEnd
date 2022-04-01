cd3718f53092f870ee090cbec269777d4e7f1c2a
DJGARG7 <18bce052@nirmauni.ac.in>
2022-04-01 21:57:45 +0530

    sale billing starts

--- src/pages/UserPages/SaleBilling.js
+++ src/pages/UserPages/SaleBilling.js
@@ -1,10 +1,91 @@
-import React from 'react';
-
+import StickyTable from "../../components/Reuse_components/Table/StickyTable";
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import {
+    toastError,
+    toastSuccess,
+} from "../../components/Reuse_components/toastError";
+import "./temp.css";
 function SaleBilling() {
-  return (
-      
-    <div><h2>Sale Billing</h2>Here pending sale challans will be displayed. Either bill it or delete it.</div>
-  )
+    const [salesList, setSalesList] = useState([]);
+    const [salesDetailList, setSalesDetailList] = useState([]);
+    useEffect(async () => {
+        try {
+            const res = await Axios.get(
+                `http://localhost:3005/sales/sales_order`
+            );
+            setSalesList(res.data);
+        } catch (e) {
+            toastError("Error loading sales data");
+        }
+    }, []);
+    const TableColData = [
+        {
+            Header: "Action",
+            Cell: (tableProps) => (
+                <button onClick={() => salesShowHandler(tableProps.row)}>
+                    Show
+                </button>
+            ),
+        },
+        {
+            Header: "Challan No.",
+            accessor: "BILL_NO",
+            Filter: "",
+        },
+        {
+            Header: "Customer",
+            accessor: "CNAME",
+            Filter: "",
+        },
+        {
+            Header: "Date",
+            accessor: "ORDER_DATE",
+            Filter: "",
+        },
+    ];
+    const TableColData1 = [
+        {
+            Header: "Design Name",
+            accessor: "NAME",
+            Filter: "",
+        },
+        {
+            Header: "Rate",
+            accessor: "RATE",
+            Filter: "",
+        },
+        {
+            Header: "Quantity",
+            accessor: "QTY",
+            Filter: "",
+        },
+    ];
+    const salesShowHandler = async (currRow) => {
+        const res = await Axios.get(
+            `http://localhost:3005/sales/sales_detail/${currRow.values.BILL_NO}`
+        );
+        console.log(res.data);
+        setSalesDetailList(res.data);
+    };
+    return (
+        <div className="sales_main">
+            <div className="sales_inside">
+                <StickyTable
+                    TableCol={TableColData}
+                    TableData={salesList}
+                ></StickyTable>
+            </div>
+            <div className="sales_inside">
+                <h2>Sale Billing</h2>Here pending sale challans will be
+                displayed. Either bill it or delete it.
+                <StickyTable
+                    TableCol={TableColData1}
+                    TableData={salesDetailList}
+                ></StickyTable>
+            </div>
+        </div>
+    );
 }
 
 export default SaleBilling;
